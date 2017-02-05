a2047884a93e7db63da254e4ad4e5f43e34cb21c
Brandon Saunders <brandon.saunders@hulu.com>
2017-02-05 04:19:06 -0800

    Test connecting to DB

--- app.js
+++ app.js
@@ -14,13 +14,59 @@
 'use strict';
 
 const express = require('express');
+const google = require('googleapis')
+const mysql = require('mysql');
 
 const app = express();
+
+/*
+foobar: () => {
+  google.auth.getApplicationDefault(function(err, authClient) {
+    if (err) {
+      return cb(err);
+    }
+
+    if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+          //authClient = authClient.createScoped(['https://www.googleapis.com/auth/devstorage.read_write']);
+          console.log("YubNub");
+    }
+
+    var storage = google.storage('v1');
+    storage.buckets.list({
+        auth: authClient,
+        project: projectId
+      },  );
+  });
+}
+*/
+
 
 // [START hello_world]
 // Say hello!
 app.get('/', (req, res) => {
-  res.status(200).send('Kazowie!!');
+  //res.status(200).send('Kazowie!!');
+  const _user = encodeURIComponent(process.env.MYSQL_USER);
+  const _password = encodeURIComponent(process.env.MYSQL_PASSWORD);
+  const _database = encodeURIComponent(process.env.MYSQL_DATABASE);
+  const _host = encodeURIComponent(process.env.INSTANCE_CONNECTION_NAME)
+
+  const uri = `mysql://${user}:${password}@${host}/${database}`;
+  var connection = mysql.createConnection({
+    host     : _host,
+    user     : _user,
+    password : _password,
+    database : _database
+  });
+  
+  connection.connect();
+
+  connection.query('SELECT * from politicians', function (error, results, fields) {
+    if (error) res.status(200).send(error);
+    //console.log('The solution is: ', results[0].solution);
+    res.status(200).send(results[0].firstName + ", " + results[0].lastName + " - " + results[0].party)
+  });
+
+  connection.end();
 });
 // [END hello_world]
 
